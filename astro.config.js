94ea4d5cbb695d76d842bbf827a27f99cac0e4c6
Lee In Seok <dlstjr9068@gmail.com>
2024-04-25 11:44:41 +0900

    헤드리스 UI 추가 및 Vue 지원 추가

--- astro.config.js
+++ astro.config.js
@@ -3,18 +3,19 @@ import { fileURLToPath } from 'url';
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-
+import vue from "@astrojs/vue";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://inseok9068.github.io',
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), vue()],
   vite: {
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src'),
-      },
-    },
-  },
+        '@': path.resolve(__dirname, './src')
+      }
+    }
+  }
 });
